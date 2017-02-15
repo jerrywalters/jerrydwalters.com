f6ee972bf934477d5e92802669ae45951e327900
Jerry <jerrydwalters@gmail.com>
2017-02-15 14:20:36 -0500

    header and gradient color tweaking

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -22,14 +22,14 @@ const Background = () => {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
+  // function getRandomColor() {
+  //   var letters = '0123456789ABCDEF';
+  //   var color = '#';
+  //   for (var i = 0; i < 6; i++ ) {
+  //       color += letters[Math.floor(Math.random() * 16)];
+  //   }
+  //   return color;
+  // }
 
   function generateShapes() {
     const windowWidth = window.innerWidth;
